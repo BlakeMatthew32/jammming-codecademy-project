f4d70249e2159bf4e788f9d02f8b4b486db7b5b2
Blake <79722560+BlakeMatthew32@users.noreply.github.com>
2025-05-27 22:38:31 +0100

    Initial Commit and file setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Jammming project setup</h1>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </>
+  )
+}
+
+export default App
