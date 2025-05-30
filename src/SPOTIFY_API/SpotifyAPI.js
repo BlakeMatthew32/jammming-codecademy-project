ebc261716421baa256e25f5b3190fa543a1d5b83
Blake <79722560+BlakeMatthew32@users.noreply.github.com>
2025-05-30 23:14:50 +0100

    start work on the spotify API interaction

---
+++ src/SPOTIFY_API/SpotifyAPI.js
@@ -0,0 +1,24 @@
+const Spotify = {
+
+    //code verifier
+    generateRandomString(length) {
+        const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurstuvwxyz0123456789"
+        const values = crypto.getRandomValues(new Uint8Array(length))
+        return values.reduce((acc, x) => acc + possible[x % possible.length], "")
+    },
+
+    // code challenge
+    async sha256(plain) {
+        const encoder = new TextEncoder()
+        const data = encoder.encode(plain)
+        return window.crypto.subtle.digest('SHA-256', data)
+    },
+
+    base64encode(input) {
+        return btoa(String.fromCharCode(...new Uint8Array(input)))
+        .replace(/=/g, '')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_');
+    }
+
+}
