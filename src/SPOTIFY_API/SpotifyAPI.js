4def4a07cebd6a860a5ceb102255b40d81e7cea1
Blake <79722560+BlakeMatthew32@users.noreply.github.com>
2025-06-01 21:30:47 +0100

    start to layout custom hook for spotify API use

--- src/SPOTIFY_API/SpotifyAPI.js
+++ src/SPOTIFY_API/SpotifyAPI.js
@@ -1,24 +1,93 @@
-const Spotify = {
+import { useState } from "react"
 
-    //code verifier
-    generateRandomString(length) {
-        const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurstuvwxyz0123456789"
-        const values = crypto.getRandomValues(new Uint8Array(length))
-        return values.reduce((acc, x) => acc + possible[x % possible.length], "")
-    },
+const codeVerifierLength = 64
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const redirectUri = import.meta.env.VITE_REDIRECT_URI;
+const scope = 'user-read-private user-read-email';
+const authUrl = new URL("https://accounts.spotify.com/authorize")
 
-    // code challenge
-    async sha256(plain) {
-        const encoder = new TextEncoder()
-        const data = encoder.encode(plain)
-        return window.crypto.subtle.digest('SHA-256', data)
-    },
+//code verifier
+function generateRandomString(length) {
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurstuvwxyz0123456789"
+    const values = crypto.getRandomValues(new Uint8Array(length))
+    return values.reduce((acc, x) => acc + possible[x % possible.length], "")
+}
 
-    base64encode(input) {
-        return btoa(String.fromCharCode(...new Uint8Array(input)))
-        .replace(/=/g, '')
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_');
+// code challenge
+async function sha256(plain) {
+    const encoder = new TextEncoder()
+    const data = encoder.encode(plain)
+    return window.crypto.subtle.digest('SHA-256', data)
+}
+
+function base64encode(input) {
+    return btoa(String.fromCharCode(...new Uint8Array(input)))
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+}
+
+function requestAuth(codeChallenge) {
+    const params =  {
+    response_type: 'code',
+    client_id: clientId,
+    scope,
+    code_challenge_method: 'S256',
+    code_challenge: codeChallenge,
+    redirect_uri: redirectUri,
     }
 
+    authUrl.search = new URLSearchParams(params).toString();
+    window.location.href = authUrl.toString();
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get('code');
 }
+
+async function getToken(code){
+
+  // stored in the previous step
+  const codeVerifier = localStorage.getItem('code_verifier');
+
+  const url = "https://accounts.spotify.com/api/token";
+  const payload = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      client_id: clientId,
+      grant_type: 'authorization_code',
+      code,
+      redirect_uri: redirectUri,
+      code_verifier: codeVerifier,
+    }),
+  }
+
+  const body = await fetch(url, payload);
+  const response = await body.json();
+
+  localStorage.setItem('access_token', response.access_token);
+}
+
+
+
+const useSpotifyAPI = () => {
+    const [codeVerifier, setCodeVerifier] = useState(generateRandomString(codeVerifierLength))
+    const [hashed, setHashed] = useState(sha256(codeVerifier))
+    const [codeChallenge, setCodeChallenge] = useState(base64encode(hashed))
+
+    // generated in the previous step
+    window.localStorage.setItem('code_verifier', codeVerifier);
+
+    
+
+
+}
+
+
+// Return some functions maybe as an object
+    // AuthOnLogin
+    // searchTracks
+    // getUserData
+    // logout
+    // sendPlaylistToProfile
